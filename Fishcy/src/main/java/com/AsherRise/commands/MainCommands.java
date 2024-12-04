99e39c18b10f4f00f49f53c4b8022ebfdbc1f04c
Rise2477 <oxasharp@gmail.com>
2024-12-05 02:45:19 +0700

    Add Tab Complete Test

--- Fishcy/src/main/java/com/AsherRise/commands/MainCommands.java
+++ Fishcy/src/main/java/com/AsherRise/commands/MainCommands.java
@@ -1,15 +1,21 @@
 package com.AsherRise.commands;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
 import com.AsherRise.Main;
 import com.AsherRise.config.ConfigManager;
 
 import net.md_5.bungee.api.ChatColor;
-public class MainCommands implements CommandExecutor {
+
+public class MainCommands implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -38,5 +44,20 @@ public class MainCommands implements CommandExecutor {
         }
 
         return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> completions = new ArrayList<>();
+        
+        if (command.getName().equalsIgnoreCase("fishcy")) {
+            if (args.length == 1) {
+                if (sender.hasPermission("fishcy.reload")) {
+                    completions.add("reload");
+                }
+            }
+        }
+        Collections.sort(completions);
+        return completions;
     }
 }
